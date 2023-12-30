114998b7407a42d2fc708d5e700dbf4fb3f46e5d
Jonas Müller <jonas.mueller.jm@gmx.de>
2023-12-30 20:57:18 +0100

    database role models

---
+++ models/items.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const inventorySchema = new Schema({
+    item: {
+        type: String,
+        required: true,
+    },
+    buy: {
+        type: Number,
+        required: true,
+    },
+    sell: {
+        type: Number,
+        required: true,
+    },
+    platform: {
+        type: String,
+        required: true,
+    },
+    buyindate: {
+        type: Date,
+        required: false,
+    },
+    selldate: {
+        type: Date,
+        required: false,
+    },
+    memo: {
+        type: String,
+        required: false,
+    },
+});
+
+const Items = mongoose.model('Items', inventorySchema);
+module.exports = Items;
